478da3dd4e9ae9616906dc23fa06c09603e6a432
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-01-29 13:50:12 +0100

    created a template for the time slots, finished step 1 and 2 of my guidlines

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # Synopsis
 
-This is a test to create a booking API. We'll be using doctolib as an example. 
+This is a test to create a booking API. We'll be using doctolib as an example.
 
-***A Doctor does not have enough time to answer all his phone calls to take new appointments.*** 
+**_A Doctor does not have enough time to answer all his phone calls to take new appointments._**
 
 ### Guidelines
 
-1. Develop a web service to obtain all the availabilities from the medical cabinet for a specific date. 
-* URL: http://localhost:3000/visits
-* Method used: GET
-* Query params: "date" (example: "2019-01-31")
-* GET: http://localhost:3000/visits?date=2019-01-31 will return a status 200 with an object including all the times slots available for this date. 
+1. Develop a web service to obtain all the availabilities from the medical cabinet for a specific date.
 
-2. The second web service will be used to book a time slot. 
-* URL: http://localhost:3000/visits 
-* Method used: POST
-* Query params: 
-  * "date" (example: "2019-01-31")
-  * "slot" (example: "1500" for 3pm)
-  * "name" (example: "John")
-* POST : we will post an object to http://localhost:3000/visits using the query params specified above. It will return a status 200 with a message saying "successfully booked"
-* If the time slot is unaivalable, we will receive a status 400 with the message "slot already booked". 
+- URL: http://localhost:3000/visits
+- Method used: GET
+- Query params: "date" (example: "2019-01-31")
+- GET: http://localhost:3000/visits?date=2019-01-31 will return a status 200 with an object including all the times slots available for this date.
+
+2. The second web service will be used to book a time slot.
+
+- URL: http://localhost:3000/visits
+- Method used: POST
+- Query params:
+  - "date" (example: "2019-01-31")
+  - "slot" (example: "1500" for 3pm)
+  - "name" (example: "John")
+- POST : we will post an object to http://localhost:3000/visits using the query params specified above. It will return a status 200 with a message saying "successfully booked"
+- If the time slot is unaivalable, we will receive a status 400 with the message "slot already booked".
+
+🚧 Functionalities to come ...
+
+- Use npm date-fns to make sure a client cannot reserve a date in the past.
+- Cannot book a slot on a Sunday
+- Create a web service to cancel a reservation.
 
 ## Directory Structure
 
@@ -31,22 +39,23 @@ doctolib_api
 ├── .gitignore
 ├── index.js
 ├── package.json
+├── slotsTemplate.json
 └── README.md
 
 ```
 
 ## Built With
 
-* [JavaScript](https://developer.mozilla.org/bm/docs/Web/JavaScript)
-* [Node.js](https://nodejs.org/en/)
-* [Express.js](https://expressjs.com/)
-* [Postman](https://www.getpostman.com/)
+- [JavaScript](https://developer.mozilla.org/bm/docs/Web/JavaScript)
+- [Node.js](https://nodejs.org/en/)
+- [Express.js](https://expressjs.com/) -> Create a robust API
+- [Postman](https://www.getpostman.com/) -> Improves API development
 
 ## Dependencies
 
-* [body-parser](https://www.npmjs.com/package/body-parser)
-* [Nodemon](https://www.npmjs.com/package/nodemon)
+- [body-parser](https://www.npmjs.com/package/body-parser)
+- [Nodemon](https://www.npmjs.com/package/nodemon)
 
 ## Acknowledgments
 
-* This is part of a FullStack Developer Bootcamp [@Le Reacteur](https://www.lereacteur.io)
+- This is part of a FullStack Developer Bootcamp [@Le Reacteur](https://www.lereacteur.io)
--- index.js
+++ index.js
@@ -2,92 +2,95 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express(); // Initializing the server
-app.use(bodyParser.json()); // This will enable us to obtain the params transmetted through the POST Method. 
+app.use(bodyParser.json()); // This will enable us to obtain the params transmetted through the POST Method.
 
-// Default json files to try the api. 
-const bookingModel = require('./bookingModel.json');
+// Default json files to try the api.
+const slotsTemplate = require("./slotsTemplate.json");
 
-// Array in which we'll push the new bookings. 
+// Array in which we'll push the new bookings.
 const booking = [];
 
 // ===== ROUTES ======= //
 
 // http://localhost:3000/
 app.get("/", (req, res) => {
-  console.log('Hello homepage');
   res.send("Welcome to the doctolib api");
-})
+});
 
 // http://localhost:3000/visits METHOD GET
 app.get("/visits", (req, res) => {
-  let checkDate = req.query.date;
-  let defaultBooking = { ...bookingModel
-  }; // make a copy of bookingModel
-  if (booking.length === 0) {
-    defaultBooking.date = checkDate;
-    booking.push(defaultBooking);
-  }
+  const checkDate = req.query.date;
+
   for (let i = 0; i < booking.length; i++) {
     if (booking[i].date === checkDate) {
       return res.json(booking[i]);
     }
   }
 
-  defaultBooking.date = checkDate;
-  booking.push(defaultBooking);
-  console.log('my booking array', booking);
+  const newEntry = {
+    date: checkDate,
+    slots: { ...slotsTemplate
+    }
+  };
 
-  res.json(defaultBooking);
-})
+  booking.push(newEntry);
+
+  res.json(newEntry);
+});
 
 // http://localhost:3000/visits METHOD POST
 app.post("/visits", (req, res) => {
   const checkDate = req.body.date;
   const slot = req.body.slot;
   const name = req.body.name;
-  let defaultBooking = { ...bookingModel
-  }; // make a copy of bookingModel
-
   const success = {
-    "message": "Successfuly booked"
+    message: "Successfuly booked"
   };
-
   const failure = {
-    "error": {
-      "message": "Slot already booked"
+    error: {
+      message: "Slot already booked"
     }
   };
-
-  if (booking.length === 0) {
-    defaultBooking.date = checkDate;
-    defaultBooking.slots[slot]["isAvailable"] = false;
-    defaultBooking.slots[slot].name = name;
-    booking.push(defaultBooking);
-    return res.json(success);
-  }
 
   for (let i = 0; i < booking.length; i++) {
-    if (booking[i].date === checkDate && booking[i].slots[slot]["isAvailable"] === true) {
-      booking[i].slots[slot]["isAvailable"] = false;
-      booking[i].slots[slot].name = name;
-      return res.json(success);
-    } else if (booking[i].date === checkDate && booking[i].slots[slot]["isAvailable"] === false) {
-      return res.json(failure);
+    if (booking[i].date === checkDate) {
+      if (booking[i].slots[slot]["isAvailable"] === true) {
+        const tempObj = {
+          isAvailable: false,
+          name: name
+        }
+        booking[i].slots[slot] = tempObj;
+        return res.json(success);
+      } else {
+        return res.json(failure);
+      }
     }
   }
 
-  return res.json(failure);
+  const newEntry = {
+    date: checkDate,
+    slots: { ...slotsTemplate
+    }
+  };
 
-})
+  newEntry.slots[slot] = {
+    name: name,
+    isAvailable: false
+  }
+
+  booking.push(newEntry);
+
+  res.json(success);
+});
 
 // ============= //
 
 // Manage pages not found
 app.all("*", function (req, res) {
-  res.status(400).send('Page not found');
+  res.status(400).send("Page not found");
 });
 
 // Choosing the port to listen
 app.listen(3000, () => {
   console.log("Server has started");
-})
+});
